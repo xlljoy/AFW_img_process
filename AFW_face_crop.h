b282c0a25710c1bee2c3d19259286be39430dd32
xileli <xll_joy@163.com>
2016-09-30 17:23:57 -0400

    change the class

---
+++ AFW_face_crop.h
@@ -0,0 +1,68 @@
+//
+// Created by xileli on 9/27/16.
+//
+
+#ifndef AFW_IMG_PROCESS_AFW_FACE_CROP_H
+#define AFW_IMG_PROCESS_AFW_FACE_CROP_H
+
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+class convert2lmdb {
+
+    vector<vector<int>> afw_face_num_;
+    vector<vector<Rect>> afw_face_rec_;
+    vector<vector<string>> afw_face_path_;
+    vector<vector<Mat>>afw_face_img;
+
+
+
+    vector<int> Num_face_;
+    vector<Rect> Rect_face_;
+    vector<Mat> face_small_;
+    vector<string> Path_full_;
+    vector<Mat> images_;
+    vector<string>mid_file_list_;
+
+
+public:
+
+    void get_mid_file_list(string mid_file_list_txt, string headpath);
+    void rec_read(string headpath);
+    void num_read(string headpath);
+    void img_path_read(string headpath);
+    vector<Mat> img_Mat_read(string headpath, int i);
+    float IoM(Rect rect_1, Rect rect_2);
+    int get_nonface_dimension(vector<Rect> rects);
+    vector<Rect> get_nonface_rect(Mat img, int width, vector<Rect>rects, float threshold);
+    void txt2jpg(string headpath, string headpath_img, string mid_file_list_txt, string smallface_output_path, float threshold, string nonface_output_path);
+
+
+
+
+    void drawfaceRect(Mat face, vector<Rect> rect);
+
+    void txtRead();
+    void txtRead(string head_path, string smallface_output_path, string mid_file_list_txt);
+    string convert_path(string head_path,string mid_file_list, string path);
+    Rect convert_rect(string path);
+    int convert_num(string path);
+    Mat readImg(string path);
+    Mat get_smallface(Mat img, Rect_<float> rect);
+    void translation();
+    void convert_JPG(Mat img, int num, string smallface_output_path);
+    void JPG_txt(string smallface_from,string smallface_output_path);
+    void JPG_datum(string path);
+    void datum_lmdb(string path);
+    void txt_lmdb(string head_path, string smallface_output_path,string mid_file_list_txt);
+
+};
+
+
+#endif //AFW_IMG_PROCESS_AFW_FACE_CROP_H
--- main.cpp
+++ main.cpp
@@ -1,37 +1,18 @@
 //
-//  main.cpp
-//  caffe_trainingdata0827
+// Created by xileli on 9/27/16.
 //
-//  Created by LiXile  on 2016-08-27.
-//  Copyright © 2016 LiXile . All rights reserved.
-//
+#include "AFW_face_crop.h"
 
-#include <iostream>
-#include "face_crop.hpp"
-
-int main() {
+int main()
+{
     convert2lmdb test;
-    string head_path = "/Users/lixile/Downloads/v1/";
-    string mid_file_list_txt = "name_list.txt";
-    string headpath_img = "/Users/lixile/Downloads/WIDER_train/images/";
-    string smallface_output_path = "/Users/lixile/Downloads/WIDER_train/small_face/";
-    string small_nonface_output_path = "/Users/lixile/Downloads/WIDER_train/small_nonface/";
+    std::string head_path = "/home/xileli/Documents/dateset/AFW/v1/";
+    std::string mid_file_list_text = "name_list.txt";
+    std::string headpath_img = "/home/xileli/Documents/dateset/AFW/WIDER_train/images/";
+    std::string smallface_output_path = "/home/xileli/Documents/dateset/AFW/WIDER_train/small_face/";
+    std::string small_nonface_output_path = "/home/xileli/Documents/dateset/AFW/WIDER_train/small_nonface/";
     int threshold = 0.1;
-    //test.txt_lmdb("/Users/lixile/Downloads/WIDER_train/images/",  "/Users/lixile/Documents/Programming/AFW_face_crop_0916", "/Users/lixile/Documents/Programming/AFW_face_crop_0916/name_list.txt");
-//    test.get_mid_file_list(mid_file_list_txt, head_path);
-//    test.num_read(head_path);
-//    test.rec_read(head_path);
-//    test.img_path_read(head_path);
-    //test.img_Mat_read(headpath_img);
-    test.txt2jpg(head_path, headpath_img,  mid_file_list_txt, smallface_output_path, threshold, small_nonface_output_path);
-    int i=0;
-//    Rect a(1, 2, 50, 50);
-//    Rect b(35, 20, 80, 50);
-//    vector<Rect> c;
-//    c.push_back(a);
-//    c.push_back(b);
-//    int d = test.get_nonface_dimension(c);
-//    float m = test.IoM(a, b);
-    
+    test.txt2jpg(head_path, headpath_img, mid_file_list_text, small_nonface_output_path, threshold, small_nonface_output_path);
     return 0;
-}
+ }
+
